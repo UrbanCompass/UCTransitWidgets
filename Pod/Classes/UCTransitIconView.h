c1a4de88d3d0cd6167e65f5fb5455708ffa24f72
James Paolantonio <james.paolantonio@urbancompass.com>
2015-01-22 11:47:23 -0500

    Added TransitIconView

---
+++ Pod/Classes/UCTransitIconView.h
@@ -0,0 +1,17 @@
+//
+//  UCTransitIconView.h
+//  Pods
+//
+//  Created by James Paolantonio on 1/22/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@class UCTransitLine;
+
+@interface UCTransitIconView : UIView
+
+- (instancetype)initWithLine:(UCTransitLine *)line;
+
+@end
---
+++ Pod/Classes/UCTransitIconView.m
@@ -0,0 +1,21 @@
+//
+//  UCTransitIconView.m
+//  Pods
+//
+//  Created by James Paolantonio on 1/22/15.
+//
+//
+
+#import "UCTransitIconView.h"
+
+@implementation UCTransitIconView
+
+- (instancetype)initWithLine:(UCTransitLine *)line {
+    self = [super init];
+    if (self) {
+        
+    }
+    return self;
+}
+
+@end
